05fd9300bbb9c736ad89dc6ecb3512de48918ded
bashmisha710 <bashmisha710@gmail.com>
2023-05-15 11:59:40 +0300

    add architecture and router

---
+++ src/app/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles/index.scss'
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <AppRouter/>
+
+        </div>
+    );
+};
+
+export default App;
