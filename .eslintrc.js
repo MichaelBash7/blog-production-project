26ba55b0bf1290b8de8c4fbac0e0c684d37b14c4
bashmisha710 <bashmisha710@gmail.com>
2023-09-21 16:28:35 +0300

    fix test

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
         'i18next',
     ],
     rules: {
-        'linebreak-style': 0,
+        'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
