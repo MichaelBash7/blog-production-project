0a5145a7361d251bc7d1e7092f47ccff54542c3b
bashmisha710 <bashmisha710@gmail.com>
2023-11-21 18:40:40 +0300

    add private route with required roles

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -6,12 +6,14 @@ export {
     getUserInited,
 } from './model/selectors/getUserInited/getUserInited';
 
+export { isUserAdmin, isUserManager, getUserRoles } from './model/selectors/roleSelectors';
+
 export {
     userReducer,
     userActions,
 } from './model/slice/userSlice';
-
 export {
     UserSchema,
     User,
+    UserRole,
 } from './model/types/user';
