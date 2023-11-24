6026959038106210ffde49235112c5c6efbc81b7
bashmisha710 <bashmisha710@gmail.com>
2023-11-24 13:21:53 +0300

    add vite for dev mode

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [
+        svgr({ exportAsDefault: true }),
+        react(),
+    ],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: '/src',
+            },
+        ],
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('http://localhost:8000'),
+        __PROJECT__: JSON.stringify('frontend'),
+    },
+    css: {
+        modules: {
+            generateScopedName: '[path][name]__[local]--[hash:base64:5]',
+        },
+    },
+});
