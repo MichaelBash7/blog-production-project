6ab69218c6cec6591f0756ff01f3ea94089353b9
bashmisha710 <bashmisha710@gmail.com>
2023-11-29 14:29:13 +0300

    update RTL, add tests for router

--- src/pages/AdminPanelPage/ui/AdminPanelPage.tsx
+++ src/pages/AdminPanelPage/ui/AdminPanelPage.tsx
@@ -6,7 +6,7 @@ const AdminPanelPage = () => {
     const { t } = useTranslation('about');
 
     return (
-        <Page>
+        <Page data-testid="AdminPanelPage">
             {t('Admin panel')}
         </Page>
     );
