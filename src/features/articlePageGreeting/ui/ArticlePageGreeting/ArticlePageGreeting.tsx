31de34a031f8cf00f72b0cdfe7ef62576ac7b761
bashmisha710 <bashmisha710@gmail.com>
2023-12-04 18:36:05 +0300

    add greeting modal for articles page

---
+++ src/features/articlePageGreeting/ui/ArticlePageGreeting/ArticlePageGreeting.tsx
@@ -0,0 +1,46 @@
+import { useTranslation } from 'react-i18next';
+import { memo, useEffect, useState } from 'react';
+import { Modal } from '@/shared/ui/Modal';
+import { Text } from '@/shared/ui/Text';
+import { saveJsonSettings, useJsonSettings } from '@/entities/User';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Drawer } from '@/shared/ui/Drawer';
+import { useDevice } from '@/shared/lib/hooks/useDevice/useDevice';
+
+export const ArticlePageGreeting = memo(() => {
+    const { t } = useTranslation();
+    const [isOpen, setIsOpen] = useState(false);
+    const { isArticlesPageWasOpened } = useJsonSettings();
+    const dispatch = useAppDispatch();
+    const isMobile = useDevice();
+
+    useEffect(() => {
+        if (!isArticlesPageWasOpened) {
+            setIsOpen(true);
+            dispatch(saveJsonSettings({ isArticlesPageWasOpened: true }));
+        }
+    }, [dispatch, isArticlesPageWasOpened]);
+
+    const onClose = () => setIsOpen(false);
+
+    const text = (
+        <Text
+            title={t('Welcome to the articles page')}
+            text={t('Here you can search and read articles on various topics')}
+        />
+    );
+
+    if (isMobile) {
+        return (
+            <Drawer lazy isOpen={isOpen} onClose={onClose}>
+                {text}
+            </Drawer>
+        );
+    }
+
+    return (
+        <Modal lazy isOpen={isOpen} onClose={onClose}>
+            {text}
+        </Modal>
+    );
+});
