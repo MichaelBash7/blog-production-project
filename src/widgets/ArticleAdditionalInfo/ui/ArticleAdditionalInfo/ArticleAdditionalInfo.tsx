efb33a08f17b6906794fe6b5db69c8f293028c19
bashmisha710 <bashmisha710@gmail.com>
2023-12-08 14:21:03 +0300

    redesign article details, fixes

--- src/widgets/ArticleAdditionalInfo/ui/ArticleAdditionalInfo/ArticleAdditionalInfo.tsx
+++ src/widgets/ArticleAdditionalInfo/ui/ArticleAdditionalInfo/ArticleAdditionalInfo.tsx
@@ -1,7 +1,6 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import cls from './ArticleAdditionalInfo.module.scss';
 import { User } from '@/entities/User';
 import { HStack, VStack } from '@/shared/ui/redesigned/Stack';
 import { Avatar } from '@/shared/ui/redesigned/Avatar';
@@ -22,12 +21,7 @@ export const ArticleAdditionalInfo = memo(
         const { t } = useTranslation();
 
         return (
-            <VStack
-                gap="32"
-                className={classNames(cls.ArticleAdditionalInfo, {}, [
-                    className,
-                ])}
-            >
+            <VStack gap="32" className={classNames('', {}, [className])}>
                 <HStack gap="8">
                     <Avatar src={author.avatar} size={32} />
                     <Text text={author.username} bold />
