8d245faa22b30e673af15e306b3cbbeb56ed24a0
bashmisha710 <bashmisha710@gmail.com>
2023-12-09 13:25:52 +0300

    fix skeleton, redesign scroll

--- src/shared/lib/features/services/updateFeatureFlags.ts
+++ src/shared/lib/features/services/updateFeatureFlags.ts
@@ -13,7 +13,7 @@ export const updateFeatureFlag = createAsyncThunk<
     void,
     UpdateFeatureFlagOptions,
     ThunkConfig<string>
->('user/saveJsonSettings', async ({ userId, newFeatures }, thunkApi) => {
+>('features/updateFeatureFlags', async ({ userId, newFeatures }, thunkApi) => {
     const { rejectWithValue, dispatch } = thunkApi;
 
     const allFeatures = {
@@ -30,7 +30,7 @@ export const updateFeatureFlag = createAsyncThunk<
         );
 
         setFeatureFlags(allFeatures);
-
+        window.location.reload();
         return undefined;
     } catch (e) {
         console.log(e);
